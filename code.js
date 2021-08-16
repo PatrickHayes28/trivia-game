c19ec7372aae9ecf8098d9ce88d77f3a23d55caa
unknown <patrickh61@yahoo.com>
2021-08-16 08:58:32 -0400

    almost finished my next question button isnt working

--- code.js
+++ code.js
@@ -2,74 +2,95 @@
 // and select "Open with Live Server"
 
 // YOUR CODE HERE!
-// the start game button will have to load the first question after i fetch the data from jservice api
-//I want the questions and answers in an array and remove them from the array after they are used
-let questionsArr = [];
-let isCorrect = true;
+let questionsArr; //holds the questions from the api
+let categoryArr = []; //holds the category from the ap1
+screengameover.style.display = "none"; //hides the gameover screen
+screenquestions.style.display = "none"; //hides category screen
+let score = 0; // starts the score at 0
+let newScore; // global variable for holding the user's score
+let category; // global variable for holding the category
+let apiAnswer = ""; // global variable for the api answer starting as a empty string
+let userAnswerElement = document.getElementById("useranswer"); // gv for holding the element representing the user's answer
+let displayQuestion = document.createElement("h3"); //gv for displaying the question
+let displayCategory = document.querySelector(".category"); //gv for displaying the category
+let randomQuestion; // gv for the question
+let currentQuestion = ""; // gv for the current question starting as a empty string
+let arrayOfQuestions; //gv for the array of questions
+let newQuestion = document.querySelector(".nextgame"); // gv for getting the next question
+
+//the fetch
 let randomCategory = fetch(
   `https://jservice.io/api/category?id=${Math.floor(Math.random() * 10) + 1}`
 )
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    for (let i = 0; i < data.clues.length; i += 1) {
-      let question = data.clues[i].question;
-      questionsArr.push(question);
-    }
-    console.log(questionsArr);
+    // puts the questions in an array
+    //data.clues is the array of questions
+    arrayOfQuestions = data.clues;
+    questionsArr = Math.floor(Math.random() * arrayOfQuestions.length); //taking the index and randomizing it
+    randomQuestion = arrayOfQuestions[questionsArr]; // taking a single question out of the mix of both
+    randomQuestion[Math.floor(Math.random() * data.length)]; // gets a  single question from the random array
+    apiAnswer = randomQuestion.answer.toString().toLowerCase(); // pulling from the random array and giving whatever it decided to give
+    console.log(apiAnswer); //answer from api
+    console.log(randomQuestion); // question from api
+    currentQuestion = randomQuestion.question;
+    displayQuestion.append(currentQuestion);
+
+    category = data.title; //gets the category
+    displayCategory.innerHTML = category; //displays it on the page
+
+    let submit = document.querySelector(".submit");
+    submit.addEventListener("click", submitButton);
   });
+console.log(questionsArr);
+console.log(categoryArr);
 
 // shows the first question
-document.getElementById("start").addEventListener("click", function (e) {
-  e.preventDefault();
-  // 1. hide login screen
-  document.querySelector(".start-game").style.display = "none";
-});
-
-// 2. show categories list screen
-document.querySelector(".screen-categories").style.display = "block";
 function startGame() {
-  document.createElement("h3");
-  h3.innerHtML = questionsArr[Math.floor(Math.random() * questionsArr.length)]; //this gets a random question
-  body.append(h3);
+  document.querySelector("#startscreen").style.display = "none"; // 1. hide start game screen
+  document.querySelector("#screenquestions").style.display = null; //show the question screen
+  displayQuestion.innerHTML = currentQuestion; //displays it on the page
+  document.body.append(displayQuestion);
+  console.log(category);
+  displayCategory.innerHTML = category;
 }
+let startscreen = document.querySelector("#startscreen");
+startscreen.addEventListener("click", startGame);
 
-addEventListener("click", startGame);
-//the submit button should submit the answer
-//then answer should be checked if it's correct and then display a message if they are correct,
-document.querySelectorAll(".screen-category").forEach(function (category) {
-  category.addEventListener("click", function () {
-    // 1. hide the categories screen
-    document.querySelector(".screen-category").style.display = "none";
-  });
-});
-function submitButton() {
-  if (isCorrect === true) {
-    return nextQuestion;
+//checks the answer and displays appropriate message
+
+function submitButton(e) {
+  e.preventDefault();
+  document.querySelector("#screenquestions").style.display = "none"; //hide the question after clicking submit
+  //console.log(userAnswer);
+  console.log(apiAnswer);
+  if (userAnswerElement.value.toString().toLowerCase() === apiAnswer) {
+    document.querySelector("#startscreen").style.display = "none";
+    userAnswerElement.value = "";
+    newScore = score + 1;
+    let p = document.querySelector(".score");
+    p.innerHTML = `score ${newScore}`;
+    let h2 = document.createElement("h2");
+    h2.innerHTML = `Correct! your score is ${newScore}`;
+    document.body.append(h2);
+    newQuestion.addEventListener("click", nextQuestion);
+    document.querySelector("#screenquestions").style.display = null;
   } else {
-    return newGame();
+    newScore = 0;
+    screengameover.style.display = null;
+    startGame();
   }
 }
 
-addEventListener("click", submitButton);
-
-//then award 1 point and the game continues
-function updateScore() {
-  let score = score + 1;
-}
 //advances the game to the next question
-function nextQuestion() {}
-
-//in the event of a wrong answer the game should end and start new game button should start the game over
-//and reset the score to 0
-function newGame() {
-  updateScore = 0;
-  document.createElement("h1");
-  h1.innerText = "GAME OVER";
-  body.append(h1);
+function nextQuestion() {
+  document.querySelector("#startscreen").style.display = "none";
+  let nextQuestion = randomQuestion.question;
+  displayQuestion.innerHTML = nextQuestion; //this gets a random question
+  apiAnswer = nextQuestion;
+  console.log("it works");
 }
-
-addEventListener("click", newGame);
 
 //DIRECTIONS:MINIMUM VIABLE PRODUCT Use fetch to pull questions from the jservice api, Present a single question to the userAllow the user to respond to the questionDetermine if the user's answer was correct  awards one (1) point and continues the game An incorrect answer resets the game and resets the score to zero (0)Keep track of and display a user's score.
 
