68ee5f68c27c7d4760385c1bb27e92e1e082d325
unknown <patrickh61@yahoo.com>
2021-08-21 10:52:23 -0400

    implemented line 80 to reset the game

--- code.js
+++ code.js
@@ -6,13 +6,14 @@ let questionsArr; //holds the questions from the api
 let categoryArr = []; //holds the category from the ap1
 screengameover.style.display = "none"; //hides the gameover screen
 screenquestions.style.display = "none"; //hides category screen
+let submit; //global variable for submit button event listener
 let score = 0; // starts the score at 0
 let newScore; // global variable for holding the user's score
-let category; // global variable for holding the category
 let apiAnswer = ""; // global variable for the api answer starting as a empty string
 let userAnswerElement = document.getElementById("useranswer"); // gv for holding the element representing the user's answer
-let displayQuestion = document.createElement("h3"); //gv for displaying the question
-let displayCategory = document.querySelector(".category"); //gv for displaying the category
+let displayCategory = document.createElement("h3"); // global variable for holding the category
+let displayQuestion = document.getElementById("questionspot"); //gv for displaying the question
+let displayHeadline = document.querySelector(".headline"); //gv for displaying the hadline
 let randomQuestion; // gv for the questions
 let currentQuestion = ""; // gv for the current question starting as a empty string
 let arrayOfQuestions; //gv for the array of questions
@@ -29,7 +30,7 @@ let randomCategory = fetch(
     console.log(data);
     // puts the questions in an array
     //data.clues is the array of questions
-    getData = data;
+    getData = data; //gv for data fetched from the api
     arrayOfQuestions = data.clues;
     questionsArr = Math.floor(Math.random() * arrayOfQuestions.length); //taking the index and randomizing it
     randomQuestion = arrayOfQuestions[questionsArr]; // taking a single question out of the mix of both
@@ -37,53 +38,54 @@ let randomCategory = fetch(
     apiAnswer = randomQuestion.answer.toString().toLowerCase(); // pulling from the random array and giving whatever it decided to give
     console.log(apiAnswer); //answer from the api
     currentQuestion = randomQuestion.question;
-    displayQuestion.append(currentQuestion);
-
-    category = data.title; //gets the category
-    displayCategory.innerHTML = category; //displays it on the page
-
-    let submit = document.querySelector(".submit"); //event listener for my submit button
+    displayQuestion.append(currentQuestion); // displays the question
+    displayCategory = data.title; //gets the category and sets it to a h3 element
+    screenquestions.append(displayHeadline); // displays the headline "category" on the screenquestions
+    screenquestions.append(displayCategory); // displays the api category on screenquestions
+    submit = document.querySelector(".submit"); //event listener for my submit button
     submit.addEventListener("click", submitButton);
   });
 
 // shows the first question
 function startGame() {
   document.querySelector("#startscreen").style.display = "none"; // hide start game screen
-  document.querySelector("#screenquestions").style.display = null; //show the question screen
+  //document.querySelector("#screenquestions").style.display = null; //show the question screen
   displayQuestion.innerHTML = currentQuestion; //displays current question on the page
-  document.body.append(displayQuestion);
-  displayCategory.innerHTML = category; //displays the category
+  document.querySelector("#screenquestions").append(displayQuestion);
+  document.querySelector("#screenquestions").style.display = "block";
 }
 let startscreen = document.querySelector(".startgame");
 startscreen.addEventListener("click", startGame);
 
 //checks the answer and displays appropriate message
 function submitButton() {
-  document.querySelector("#screenquestions").style.display = "none"; //hide the question after clicking submit and show the correct message and the next question button
+  //document.querySelector("#screenquestions").style.display = "none"; //hide the question after clicking submit and show the correct message and the next question button
   if (userAnswerElement.value.toString().toLowerCase() === apiAnswer) {
     //check if the answer is correct
     document.querySelector("#startscreen").style.display = "none"; //hide the start screen
     userAnswerElement.value = ""; // make user answer a empty string
     newScore = score += 1; // give 1 point
     let p = document.querySelector(".score");
-    p.innerHTML = `score ${newScore}`; //displys score in the header
+    p.innerHTML = `score ${newScore}`; //displays score in the header
     let h2 = document.createElement("h2");
+    h2.id = "h2";
     h2.innerHTML = `Correct! your score is ${newScore}`;
     document.body.append(h2); //70-74 display a correct message and display the point onto the page
     newQuestion.addEventListener("click", nextQuestion); // get a new question
-    document.querySelector("#screenquestions").style.display = null; //show the next question
   } else {
     newScore = 0;
-    screengameover.style.display = null; //show game over screen
-    setTimeout(startGame, 1000);
-    window.location.reload();
-    newGame.addEventListener("click", startGame()); //call start game to start a new game when new game is clicked
+    document.querySelector("#screengameover").style.display = "initial"; //show the game over screen
+    displayQuestion.style.display = "none";
+    setTimeout(function () {
+      window.location.reload(); //resets the game after 3 seconds
+    }, 3000);
   }
 }
 
 //advances the game to the next question
 function nextQuestion() {
   document.querySelector("#startscreen").style.display = "none"; // hide the start  screen
+  document.querySelector("#h2").innerHTML = "";
   arrayOfQuestions = Object.keys(getData.clues); // turned array to object with key value pair
   questionsArr = Math.floor(Math.random() * arrayOfQuestions.length); //randomizing the array
   randomQuestion = getData.clues[arrayOfQuestions[questionsArr]]; //randomizing the clues array of questions and the questions array
